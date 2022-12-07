8d33a723d182b7a071a486ab4288e72fc3fbe71c
Mica Linscheid <micascheid@gmail.com>
2022-12-07 09:00:39 -0300

    Adding NomicsAPI.py to .gitignore

---
+++ DataPull.py
@@ -0,0 +1,14 @@
+"""
+DataPull Class takes the following: market, time_frame, start, end
+
+
+"""
+
+class DataPull():
+    def __init__(self, market, time_frame, start, end):
+        self.market = market
+        self.time_frame = time_frame
+        self.start = start
+        self.end = end
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+"""
+Plan for Dec7th
+Work part 1 of my backtesting engine requirments list: This revolves around data pulling from nomics
+
+Create a Data_pull class with the following parameters: Pair, timeframe,
+"""
+
+if __name__ == "__main__":
+    print("hello world")
